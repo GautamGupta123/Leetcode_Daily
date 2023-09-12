27ff6f5190c41ba40fb517f53a3842a6775d73b4
GautamGupta123 <gautamgupta100078@gmail.com>
2023-09-12 21:57:22 +0530

    sort list

---
+++ sortlist.java
@@ -0,0 +1,28 @@
+class Solution {
+    public static class ListNode{
+        int val;
+        ListNode next;
+        ListNode(int val){
+            this.val=val;
+            this.next=null;
+        }
+    }
+    public ListNode sortList(ListNode head) {
+        ListNode temp=head;
+        ListNode prev=null;
+        int data;
+        while(temp!=null){
+            prev=temp.next;
+            while(prev!=null){
+                if(temp.val>prev.val){
+                    data=temp.val;
+                    temp.val=prev.val;
+                    prev.val=data;
+                }
+                prev=prev.next;
+            }
+            temp=temp.next;
+        }
+        return head;
+    }
+}
