4a712b34ef5dcc03ceab00b1fe0907f0b609abce
GautamGupta123 <gautamgupta100078@gmail.com>
2023-09-17 14:49:34 +0530

    binary number with alternating bits leetcode

--- Binary Number with Alternating Bits.java
+++ Binary Number with Alternating Bits.java
@@ -1,21 +1,22 @@
 class Solution {
     public boolean hasAlternatingBits(int n) {
-        String str="";
+        StringBuilder str=new StringBuilder();
         while(n>0){
-            str=str+n%2;
+            str.append(n%2);
             n=n/2;
         }
         int i;
         int flag=0;
-        for(i=0;i<str.length()-1;i++){
-            if(str.charAt(i)!=str.charAt(i+1)){
+        String str1=str.toString();
+        for(i=0;i<str1.length()-1;i++){
+            if(str1.charAt(i)!=str1.charAt(i+1)){
                 flag++;
             }
             else {
                 return false;
             }
         }
-        if(flag==(str.length()-1)){
+        if(flag==(str1.length()-1)){
             return true;
         }
         return false;
