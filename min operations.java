c7da97267c89bbcaa322a592cf1ee9f424141dac
GautamGupta123 <gautamgupta100078@gmail.com>
2023-10-26 00:17:22 +0530

    min operations

---
+++ min operations.java
@@ -0,0 +1,17 @@
+class Solution
+{
+    public int minOperation(int n)
+    {
+        int op=0;
+        while(n!=0){
+            if(n%2==0){
+                n=n/2;
+            }
+            else{
+                n=n-1;
+            }
+            op++;
+        }
+        return op;
+    }
+}
