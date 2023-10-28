dd030c07afcd868410530f3bdbd549df8e62e5cd
GautamGupta123 <gautamgupta100078@gmail.com>
2023-10-28 20:59:13 +0530

    subarrays sum of sqaures leetcode

---
+++ Subarrays Distinct Element Sum of Squares I.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public int sumCounts(List<Integer> nums) {
+        int i,j;
+        HashSet<Integer>hs=new HashSet<Integer>();
+        int square=0;
+        // for(i=0;i<=nums.size()-1;i++){
+         if(nums.size()==1){
+             return 1;
+         }
+        for(i=0;i<nums.size()-1;i++){
+               square+=(int)Math.pow(1,2);
+               hs.clear();
+            for(j=i+1;j<=nums.size()-1;j++){
+                hs.add(nums.get(i));
+                hs.add(nums.get(j));
+                square=square+(int)Math.pow(hs.size(),2);
+            }
+        }
+        square=square+1;
+        return square;
+    }
+}
