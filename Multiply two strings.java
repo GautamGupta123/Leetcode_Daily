c982ddd4e7a485db4ffa6663db4eaa8ee3785eee
GautamGupta123 <gautamgupta100078@gmail.com>
2023-12-01 19:03:59 +0530

    multiply two numbers

---
+++ Multiply two strings.java
@@ -0,0 +1,12 @@
+class Solution
+{
+    public String multiplyStrings(String s1,String s2)
+    {
+        BigInteger num1 = new BigInteger(s1);
+        BigInteger num2 = new BigInteger(s2);
+        BigInteger ans = num1.multiply(num2);
+        String s = ans.toString();
+    
+    return s;
+    }
+}
